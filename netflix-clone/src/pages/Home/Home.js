81abc5b59de71d64f770fd29d0371172da396e27
janbuca <janbuca333@gmail.com>
2024-01-18 11:29:12 -0800

    The Header and the Footer part can be done

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../componets/Header/Header'
+import Footer from '../../componets/Footer/Footer'
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
