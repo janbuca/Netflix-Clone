afe7678d70c8764f3155b88c65df85b6d05fa438
janbuca <janbuca333@gmail.com>
2024-01-18 13:00:10 -0800

    It is adding Banner.js and Banner.css in the Netflix-Clone

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../componets/Header/Header'
 import Footer from '../../componets/Footer/Footer'
+import Banner from '../../Banner/Banner'
 
 function Home() {
   return (
     <div>
         <Header />
+        <Banner/>
         <Footer />
     </div>
   )
