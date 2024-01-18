26dd167d6dca1976d6491e5e1806b0af5655019d
janbuca <janbuca333@gmail.com>
2024-01-18 14:11:42 -0800

    Adding Row.js and RowLiast.js it also adding the row.css and rowlist.css

--- netflix-clone/src/componets/Header/Header.js
+++ netflix-clone/src/componets/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./header.css";
-// import NetflixLogo  from "../../assets/images"
+import NetflixLogo from "../../assets/images/10001.png";
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
@@ -12,8 +12,8 @@ function Header() {
         <div className='header_container'>
             <div className='header_left'>
                 <ul>
-                    {/* <li><img src={NetflixLogo} alt='Netflix Logo' width='100' /></li> */}
-                    <li>Netflix</li>
+                    <li><img src={NetflixLogo} alt='Netflix Logo' width='100' /></li>
+                    {/* <li>Netflix</li> */}
                     <li>Home</li>
                     <li>TVShows</li>
                     <li>Movises</li>
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../componets/Header/Header'
 import Footer from '../../componets/Footer/Footer'
 import Banner from '../../Banner/Banner'
+import RowList from '../../componets/Rows/RowList/RowList'
 
 function Home() {
   return (
     <div>
         <Header />
         <Banner/>
+        <RowList/>
         <Footer />
     </div>
   )
